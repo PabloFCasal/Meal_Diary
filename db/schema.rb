d87c62d980abd84551365f3dcf3995fbe60732b5
PabloFCasal <pablofcasal@gmail>
2017-02-01 10:39:26 +0100

    Added model portion

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170201093539) do
+ActiveRecord::Schema.define(version: 20170201093902) do
 
   create_table "foods", force: :cascade do |t|
     t.string   "name"
@@ -18,6 +18,12 @@ ActiveRecord::Schema.define(version: 20170201093539) do
     t.decimal  "carbs"
     t.decimal  "proteins"
     t.decimal  "fats"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "portions", force: :cascade do |t|
+    t.integer  "weight"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
