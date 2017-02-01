57c734eff9bc8365170432ea00dfd93e7e02ac66
PabloFCasal <pablofcasal@gmail>
2017-02-01 10:49:35 +0100

    Added model family

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170201094818) do
+ActiveRecord::Schema.define(version: 20170201094915) do
+
+  create_table "families", force: :cascade do |t|
+    t.string   "family"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "foods", force: :cascade do |t|
     t.string   "name"
