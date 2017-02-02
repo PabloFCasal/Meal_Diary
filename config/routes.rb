7f293ec7b0ca0b8a48f2756cd226edba2b3f8d5e
PabloFCasal <pablofcasal@gmail>
2017-02-02 01:01:35 +0100

    Delete porion controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'porion/index'
-
-  get 'porion/new'
-
-  get 'porion/create'
-
-  get 'porion/show'
-
-  get 'porion/view'
-
-  get 'porion/delete'
-
   root to: 'welcome#index'
 
   resources :meals
