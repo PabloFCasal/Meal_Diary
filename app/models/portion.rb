91208b250044b2ffe502a87433986e98dd4944d8
PabloFCasal <pablofcasal@gmail>
2017-02-02 01:16:42 +0100

    Model portion relationships

--- app/models/portion.rb
+++ app/models/portion.rb
@@ -1,2 +1,11 @@
 class Portion < ApplicationRecord
+
+  validates :weight, presence: true
+
+  # portion <--> meal
+  belongs_to :meal, optional: true
+
+  # portion <--> food
+  belongs_to :food, optional: true
+
 end
