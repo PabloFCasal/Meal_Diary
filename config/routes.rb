36005540b9db50782a0a7a2beec848707e11b7f4
PabloFCasal <pablofcasal@gmail>
2017-02-02 01:17:13 +0100

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'portion/index'
-
-  get 'portion/new'
-
-  get 'portion/create'
-
-  get 'portion/show'
-
-  get 'portion/view'
-
-  get 'portion/delete'
-
   root to: 'welcome#index'
 
   resources :meals
