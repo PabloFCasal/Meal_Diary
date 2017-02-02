ed44a7be5cc8bffbcfd192767fba285041999edc
PabloFCasal <pablofcasal@gmail>
2017-02-02 01:19:01 +0100

    Total new portion controller

--- app/controllers/portion_controller.rb
+++ app/controllers/portion_controller.rb
@@ -1,19 +1,38 @@
-class PortionController < ApplicationController
-  def index
+class PortionsController < ApplicationController
+
+def index
+   @portions = Portion.all
+end
+
+def new
+  @portion = Portion.new
+end
+
+def create
+  @portion = Portion.new(portion_params)
+
+  respond_to do |format|
+    if @portion.save
+       format.html {redirect_to @portion, notice: 'Portion created successfully'}
+    else
+      puts @portion.errors.inpes
+       format.html {render :new}
+    end
   end
 
-  def new
-  end
+end
 
-  def create
-  end
+def show
+  @portion = Portion.find(params[:id])
+end
+def view
+end
 
-  def show
-  end
+def destroy
+end
 
-  def view
-  end
+def portion_params
+  params.require(:portion).permit(:weight, :food_id)
+end
 
-  def delete
-  end
 end
