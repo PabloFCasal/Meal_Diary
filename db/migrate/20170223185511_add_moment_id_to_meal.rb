a833345ce57e934f3ff229b22de9957747c4f42f
PabloFCasal <pablofcasal@gmail>
2017-02-23 21:16:20 +0100

    Deleted last 2 migrations

--- db/migrate/20170223185511_add_moment_id_to_meal.rb
+++
@@ -1,9 +0,0 @@
-class AddMomentIdToMeal < ActiveRecord::Migration[5.0]
-  def self.up
-    add_index("meals", ["moment_id"])
-  end
-
-  def self.down
-    remove_index("meals", ["moment_id"])
-  end
-end
